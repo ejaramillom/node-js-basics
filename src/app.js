d8cfcfb81b0e44b52abed75ae6b42224196f5a69
Emmanuel <ejaramilo@hotmail.com>
2023-01-17 10:16:04 -0500

    004 npm manager and dependencies

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const { v4: uuidv4 } = require("uuid");
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -8,4 +9,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, "127.0.0.1", () => {
   console.log("Server is running in port 3000...");
+  console.log(uuidv4());
 });
