ec86e4ed805b98b1486500ef6b0aa26192464044
Emmanuel <ejaramilo@hotmail.com>
2023-01-17 09:57:04 -0500

    003 creating a project and writing scripts

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("content-type", "text/html");
+  res.end("<h1> Hello world </h1>");
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server is running in port 3000...");
+});
